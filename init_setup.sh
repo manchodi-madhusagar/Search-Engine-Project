cb78aa89dd2bd6a91ab5a72d31470b95b5a9974f
manchodi-madhusagar <sagarmadhu029@gmail.com>
2024-06-17 08:38:06 +0530

    update code structure

--- init_setup.sh
+++ init_setup.sh
@@ -2,7 +2,7 @@ echo [$(date)]: "START"
 echo [$(date)]: "create env with python 3.11.7 version"
 conda create --predfix ./venvs  python=3.11.7 -y
 echo [$(date)]: "activate env"
-source activate ./venv
+source activate ./venvs
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt
 echo [$(date)]: "END"
